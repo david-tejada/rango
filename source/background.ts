5c08818acaa2fafe21745d2708d87173cfe380ee
David Tejada <davidxtejada@gmail.com>
2022-03-30 12:22:26 +0200

    Add insert-viewport-mark command

--- source/background.ts
+++ source/background.ts
@@ -2,7 +2,7 @@
 import "./options-storage";
 import * as browser from "webextension-polyfill";
 
-// This gets triggered whenever a command is executed, it has to be on a background
+// 	This gets triggered whenever a command is executed, it has to be on a background
 // script.
 browser.commands.onCommand.addListener((command) => {
 	console.log(command);
@@ -17,4 +17,31 @@ browser.runtime.onMessage.addListener(async (data, sender): Promise<string> => {
 	console.log("Sender:\n");
 	console.log(sender);
 	return "Background: Message from 'content' received";
+});
+
+const port = browser.runtime.connectNative("rango");
+console.log(port);
+
+port.onMessage.addListener((response) => {
+	console.log(response);
+	// browser.tabs.executeScript({
+	//   code: getClickScript(response.target),
+	// });
+});
+
+browser.commands.onCommand.addListener((command) => {
+	if (command === "get-talon-request") {
+		console.log("Sending:  ping");
+		port.postMessage("ping");
+	}
+
+	if (command == "insert-viewport-mark") {
+		browser.tabs
+			.executeScript({
+				file: "insert-viewport-mark.js",
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 });
