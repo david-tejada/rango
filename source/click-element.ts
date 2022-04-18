8e03f4e0daf722de4e3c658e407d0d363726929a
David Tejada <davidxtejada@gmail.com>
2022-04-18 10:38:50 +0200

    Remove clickElementByText and showAllHints commands

--- source/click-element.ts
+++ source/click-element.ts
@@ -1,27 +1,4 @@
-import { displayHints, clearHints } from "./hints";
 import { observedElements } from "./observed-elements";
-
-export function clickElementByText(text: string): void {
-	clearHints();
-	const choices = observedElements.filter((observedElement) => {
-		const textToSearch = text.toLowerCase().replace(" ", "");
-		const textSearched = observedElement.element
-			.textContent!.toLowerCase()
-			.replace(" ", "");
-		return (
-			textSearched.includes(textToSearch) &&
-			observedElement.isIntersecting &&
-			observedElement.isVisible &&
-			observedElement.clickableType
-		);
-	});
-
-	if (choices.length > 1) {
-		displayHints({ text });
-	} else if (choices[0]) {
-		clickElement(choices[0].element as HTMLElement);
-	}
-}
 
 export function clickElementByHint(hintNumber: number) {
 	const target = observedElements.find(
@@ -33,7 +10,6 @@ export function clickElementByHint(hintNumber: number) {
 }
 
 function clickElement(element: HTMLElement) {
-	clearHints();
 	const previousOutline = element.style.outline;
 	element.style.outline = "#247881 solid 2px";
 	setTimeout(() => {
--- source/content.ts
+++ source/content.ts
@@ -1,17 +1,8 @@
 import browser from "webextension-polyfill";
-import { clickElementByText, clickElementByHint } from "./click-element";
-import { displayHints } from "./hints";
+import { clickElementByHint } from "./click-element";
 
 browser.runtime.onMessage.addListener(async (request) => {
-	if (request.action.type === "clickElementByText") {
-		clickElementByText(request.action.target);
-	}
-
 	if (request.action.type === "clickElementByHint") {
 		clickElementByHint(request.action.target);
-	}
-
-	if (request.action.type === "showAllHints") {
-		displayHints();
 	}
 });
