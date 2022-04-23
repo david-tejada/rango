ece46bf1e0efdd952a2b89e7a540757c626f2877
David Tejada <davidxtejada@gmail.com>
2022-04-23 11:55:40 +0200

    Fix hover

--- src/content/hover.ts
+++ src/content/hover.ts
@@ -1,7 +1,19 @@
 import { intersectingElements } from "./intersecting-elements";
 import { displayHints } from "./hints";
 
+const hoveredElements: Set<Element> = new Set();
+const timeoutIds: Set<NodeJS.Timeout> = new Set();
+
 export function hoverElementByHint(hintNumber: number, fixed: boolean) {
+	hoveredElements.clear();
+	for (const timeoutId of timeoutIds) {
+		clearTimeout(timeoutId);
+	}
+
+	for (const hoveredElement of hoveredElements) {
+		unhoverElement(hoveredElement);
+	}
+
 	const target = intersectingElements.find(
 		(intersectingElement) => intersectingElement.hintText === String(hintNumber)
 	);
@@ -14,19 +26,21 @@ export function hoverElementByHint(hintNumber: number, fixed: boolean) {
 		});
 
 		targetElement.dispatchEvent(event);
+		hoveredElements.add(targetElement);
 		displayHints(intersectingElements);
 
 		if (!fixed) {
-			setTimeout(() => {
+			const timeoutId = setTimeout(() => {
 				unhoverElement(targetElement);
 			}, 10_000);
+			timeoutIds.add(timeoutId);
 		}
 	}
 }
 
 export function unhoverAll() {
-	for (const intersectingElement of intersectingElements) {
-		unhoverElement(intersectingElement.element);
+	for (const hoveredElement of hoveredElements) {
+		unhoverElement(hoveredElement);
 	}
 }
 
