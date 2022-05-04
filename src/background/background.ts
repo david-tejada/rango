56887ce1fbf501163644214cd8b00492eb91ee34
David Tejada <davidxtejada@gmail.com>
2022-05-04 11:58:15 +0200

    Chrome support through execCommand

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,6 +1,9 @@
 import browser from "webextension-polyfill";
 import { Message } from "../types/types";
 
+const sandbox = document.createElement("textarea");
+document.body.append(sandbox);
+
 browser.commands.onCommand.addListener(async (command: string) => {
 	if (command === "get-talon-request") {
 		try {
@@ -19,8 +22,16 @@ browser.commands.onCommand.addListener(async (command: string) => {
 });
 
 async function getMessageFromClipboard(): Promise<Message> {
-	const clipText = await navigator.clipboard.readText();
-	const request = JSON.parse(clipText) as Message;
+	let clipText: string;
+	try {
+		clipText = await navigator.clipboard.readText();
+	} catch (error: unknown) {
+		if (error instanceof DOMException) {
+			clipText = getClipboard();
+		}
+	}
+
+	const request = JSON.parse(clipText!) as Message;
 	if (request.type !== "request") {
 		throw new Error("Error: No request message present in the clipboard");
 	}
@@ -32,7 +43,13 @@ async function writeResponseToClipboard(responseObject: Message) {
 	// We send the response so that talon can make sure the request was received
 	// and to tell talon to execute any actions
 	const response = JSON.stringify(responseObject);
-	await navigator.clipboard.writeText(response);
+	try {
+		await navigator.clipboard.writeText(response);
+	} catch (error: unknown) {
+		if (error instanceof DOMException) {
+			copyTextToClipboard(response);
+		}
+	}
 }
 
 async function sendMessageToActiveTab(message: Message): Promise<Message> {
@@ -55,3 +72,21 @@ browser.runtime.onMessage.addListener(async (message) => {
 		});
 	}
 });
+
+function getClipboard() {
+	let result = "";
+	sandbox.focus();
+	if (document.execCommand("paste")) {
+		result = sandbox.value;
+	}
+
+	sandbox.value = "";
+	return result;
+}
+
+function copyTextToClipboard(text: string) {
+	sandbox.value = text;
+	sandbox.select();
+	document.execCommand("copy");
+	sandbox.value = "";
+}
