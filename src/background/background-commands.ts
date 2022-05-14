ed1062868b2915411baa1908162cb904f2fab738
David Tejada <davidxtejada@gmail.com>
2022-05-14 13:19:03 +0200

    Background commands refactor

--- src/background/background-commands.ts
+++ src/background/background-commands.ts
@@ -1,23 +1,10 @@
 import { Command } from "../types/types";
 import { sendCommandToAllTabs } from "./tabs-messaging";
 
-async function toggleHintsInAllTabs() {
-	try {
-		await sendCommandToAllTabs({ type: "toggleHints" });
-	} catch (error: unknown) {
-		let errorMessage = "Error: There was an error";
-		if (error instanceof Error) {
-			errorMessage = error.message;
-		}
-
-		console.error(errorMessage);
-	}
-}
-
 export async function executeBackgroundCommand(command: Command) {
 	switch (command.type) {
 		case "toggleHints":
-			await toggleHintsInAllTabs();
+			await sendCommandToAllTabs({ type: "toggleHints" });
 			break;
 		default:
 			break;
