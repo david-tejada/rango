8cb89aea3791472b836ef176ba1695c46be20328
David Tejada <davidxtejada@gmail.com>
2022-05-17 11:02:34 +0200

    Fix typed text leaking into copyPasteArea of chromium clipboard

--- src/content/chromium-clipboard.ts
+++ src/content/chromium-clipboard.ts
@@ -1,5 +1,9 @@
 const copyPasteArea = document.createElement("textarea");
 copyPasteArea.id = "rango-copy-paste-area";
+copyPasteArea.tabIndex = -1;
+// We need to disable the textarea to avoid anything we type to go into the textarea,
+// which would leak into or copied text and break JSON.parse
+copyPasteArea.disabled = true;
 copyPasteArea.style.position = "fixed";
 copyPasteArea.style.left = "0";
 copyPasteArea.style.top = "0";
@@ -8,18 +12,22 @@ document.body.append(copyPasteArea);
 
 export function getChromiumClipboard(): string {
 	let result = "";
+	copyPasteArea.disabled = false;
 	copyPasteArea.focus({ preventScroll: true });
 	if (document.execCommand("paste")) {
 		result = copyPasteArea.value;
 	}
 
 	copyPasteArea.value = "";
+	copyPasteArea.disabled = true;
 	return result;
 }
 
 export function copyToChromiumClipboard(text: string) {
+	copyPasteArea.disabled = false;
 	copyPasteArea.value = text;
 	copyPasteArea.select();
 	document.execCommand("copy");
 	copyPasteArea.value = "";
+	copyPasteArea.disabled = true;
 }
