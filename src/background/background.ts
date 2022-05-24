f24aa6aaa67081e661f8a4e868e2d045e3a6184b
David Tejada <davidxtejada@gmail.com>
2022-05-24 11:34:02 +0200

    Only send response back when is necessary to avoid Chromium clipboard stealing focus

--- src/background/background.ts
+++ src/background/background.ts
@@ -22,19 +22,28 @@ browser.commands.onCommand.addListener(async (internalCommand: string) => {
 		try {
 			const request = await getMessageFromClipboard();
 			let response = await dispatchCommand(request.action);
-			const changedClipboard = await getClipboardIfChanged();
-			if (changedClipboard) {
-				response = {
-					type: "response",
-					action: {
-						type: "copyToClipboard",
-						textToCopy: changedClipboard,
-					},
-				};
-			}
+			// Because of the way I had to implement copying and pasting to the clipboard in Chromium,
+			// sending a response requires focusing the textarea element dedicated for it, which might
+			// close popup elements or have other unintended consequences, therefore I will only send
+			// a response back when it's absolutely necessary. Even though for firefox this is not a problem,
+			// I will not differentiate for simplicity
+			const commandsThatRequireResponse = ["copyLink"];
+			if (commandsThatRequireResponse.includes(request.action.type)) {
+				console.log("Response required");
+				const changedClipboard = await getClipboardIfChanged();
+				if (changedClipboard) {
+					response = {
+						type: "response",
+						action: {
+							type: "copyToClipboard",
+							textToCopy: changedClipboard,
+						},
+					};
+				}
 
-			const adaptedResponse = adaptResponse(response, request.version ?? 0);
-			await writeResponseToClipboard(adaptedResponse);
+				const adaptedResponse = adaptResponse(response, request.version ?? 0);
+				await writeResponseToClipboard(adaptedResponse);
+			}
 		} catch (error: unknown) {
 			let errorMessage = "Error: There was an error";
 			if (error instanceof Error) {
