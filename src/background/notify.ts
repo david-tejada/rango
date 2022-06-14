64e42b8d4741b807258f653514022a086a565034
David Tejada <davidxtejada@gmail.com>
2022-06-14 18:44:04 +0200

    Fix system notifications

---
+++ src/background/notify.ts
@@ -0,0 +1,11 @@
+import browser from "webextension-polyfill";
+import iconUrl from "url:../assets/icon128.png";
+
+export function notify(title: string, message: string) {
+	void browser.notifications.create("rango-notification", {
+		type: "basic",
+		iconUrl,
+		title,
+		message,
+	});
+}
