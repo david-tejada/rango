75d2a2381e86f4389a26f589b4c432851e58f4f2
David Tejada <davidxtejada@gmail.com>
2022-06-30 15:24:27 +0200

    Fix url in title not updating with hash change

--- src/content/utils/url-in-title.ts
+++ src/content/utils/url-in-title.ts
@@ -1,12 +1,22 @@
 import browser from "webextension-polyfill";
 
+let lastUrl: string;
+
 export async function addUrlToTitle() {
 	const { urlInTitle } = (await browser.storage.local.get(
 		"urlInTitle"
 	)) as Record<string, boolean>;
 	if (urlInTitle) {
+		window.addEventListener("hashchange", () => {
+			if (document.title?.includes(lastUrl)) {
+				document.title = document.title.replace(lastUrl, window.location.href);
+				lastUrl = window.location.href;
+			}
+		});
+
 		if (document.title && !document.title.includes(window.location.href)) {
 			document.title = document.title + " - " + window.location.href;
+			lastUrl = window.location.href;
 		}
 
 		const headObserver = new MutationObserver(() => {
@@ -14,6 +24,7 @@ export async function addUrlToTitle() {
 			// I think it's quicker this way
 			if (document.title && !document.title.includes(window.location.href)) {
 				document.title = document.title + " - " + window.location.href;
+				lastUrl = window.location.href;
 			}
 		});
 		const config = { attributes: true, childList: true, subtree: true };
