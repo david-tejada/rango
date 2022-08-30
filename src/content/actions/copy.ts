6402ff4c090110a116f26bcfb8f427a6731b31f6
David Tejada <davidxtejada@gmail.com>
2022-08-30 12:31:19 +0200

    Fix copy actions for permanent hints rewrite

--- src/content/actions/copy.ts
+++ src/content/actions/copy.ts
@@ -1,56 +1,54 @@
-import { HintedIntersector } from "../../typings/Intersector";
+import { Hintable } from "../Hintable";
 import { showTooltip } from "../hints/showTooltip";
 
 export function copyElementTextContentToClipboard(
-	intersectors: HintedIntersector[]
+	hintables: Hintable[]
 ): string | undefined {
 	const textContents = [];
-	for (const intersector of intersectors) {
-		const textContent = intersector.element.textContent;
+	for (const hintable of hintables) {
+		const textContent = hintable.element.textContent;
 		textContents.push(textContent);
 		const message = textContent ? "Copied!" : "No text content to copy";
-		showTooltip(intersector, message, 1500);
+		showTooltip(hintable, message, 1500);
 	}
 
 	return textContents.length > 0 ? textContents.join("\n") : undefined;
 }
 
-export function copyLinkToClipboard(
-	intersectors: HintedIntersector[]
-): string | undefined {
+export function copyLinkToClipboard(hintables: Hintable[]): string | undefined {
 	const hrefs = [];
 
-	for (const intersector of intersectors) {
+	for (const hintable of hintables) {
 		let href;
-		if (intersector.element instanceof HTMLAnchorElement) {
-			href = intersector.element.href;
+		if (hintable.element instanceof HTMLAnchorElement) {
+			href = hintable.element.href;
 			hrefs.push(href);
 		}
 
 		const message = href ? "Copied!" : "Not a link";
-		showTooltip(intersector, message, 1500);
+		showTooltip(hintable, message, 1500);
 	}
 
 	return hrefs.length > 0 ? hrefs.join("\n") : undefined;
 }
 
 export function copyMarkdownLinkToClipboard(
-	intersectors: HintedIntersector[]
+	hintables: Hintable[]
 ): string | undefined {
 	const markdownLinks = [];
 
-	for (const intersector of intersectors) {
+	for (const hintable of hintables) {
 		let href;
 		let markdownLink;
-		if (intersector.element instanceof HTMLAnchorElement) {
-			href = intersector.element.href;
-			const title = intersector.element.textContent ?? "";
+		if (hintable.element instanceof HTMLAnchorElement) {
+			href = hintable.element.href;
+			const title = hintable.element.textContent ?? "";
 			markdownLink = `[${title}](${href})`;
 			markdownLinks.push(markdownLink);
 		}
 
 		const message = markdownLink ? "Copied!" : "Not a link";
-		showTooltip(intersector, message, 1500);
+		showTooltip(hintable, message, 1500);
 	}
 
 	return markdownLinks.length > 0 ? markdownLinks.join("\n") : undefined;
