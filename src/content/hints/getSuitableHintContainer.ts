2120b34458a0831bc65fb460374f8d8d1b34e469
David Tejada <davidxtejada@gmail.com>
2022-09-07 15:53:21 +0200

    Improvements on getting a suitable hint container

--- src/content/hints/getSuitableHintContainer.ts
+++ src/content/hints/getSuitableHintContainer.ts
@@ -1,15 +1,11 @@
 // This functions returns the first suitable ancestor to place the hint where it
+// won't be hidden by overflow hidden or auto. Although there are some occasions
+// where we can't assure that the hint won't be hidden by overflow hidden or auto.
 
-import { resizeObserver } from "../observers";
-
-// won't be hidden by overflow hidden or auto
 export function getSuitableHintContainer(element: Element): HTMLElement {
 	const elementRect = element.getBoundingClientRect();
-	const bodyRect = document.body.getBoundingClientRect();
-
-	// We take into account if the element is near the limit of the document
-	const minLeft = Math.min(10, elementRect.x - bodyRect.x);
-	const minTop = Math.min(10, elementRect.y - bodyRect.y);
+	const minLeft = 10;
+	const minTop = 10;
 
 	// This is the element that we are currently checking if it has sufficient
 	// space around so that the hint isn't hidden
@@ -18,36 +14,27 @@ export function getSuitableHintContainer(element: Element): HTMLElement {
 	let current = element.parentElement;
 
 	while (current) {
-		// We can't place the hint inside of a <details> element because it will be hidden
-		if (current instanceof HTMLDetailsElement) {
+		// We can't place the hint for the <summary> element inside of the <details>
+		// element because it will be hidden
+		if (
+			current instanceof HTMLDetailsElement &&
+			element.tagName === "SUMMARY"
+		) {
 			current = current.parentElement;
 			continue;
 		}
 
-		const style = window.getComputedStyle(current);
-
-		// If the current element is a scroll container we can't go beyond as we
-		// need the hint to stay within its scrolling container
-		if (
-			(current.scrollHeight > current.clientHeight ||
-				current.scrollWidth > current.clientWidth) &&
-			/scroll|auto/.test(style.overflow)
-		) {
-			resizeObserver.observe(current);
-			return current;
-		}
-
-		if (style.position === "fixed" || style.position === "sticky") {
-			resizeObserver.observe(current);
-			return current;
-		}
+		const currentStyle = window.getComputedStyle(current);
 
 		// The style.display check doesn't always ensure semantic correctness.
 		// For example, having a div inside a button is not semantically correct
 		// even if the button is display: block. I'm not sure if semantic correctness
 		// is something we should strive for, anyway. I've also tried to place hints inside
 		// inline elements but it breaks some pages (Slack, for example).
-		if (!candidate && /block|grid|flex|root/.test(style.display)) {
+		if (
+			!candidate &&
+			/block|grid|flex|root|list-item/.test(currentStyle.display)
+		) {
 			candidate = current;
 		}
 
@@ -56,19 +43,41 @@ export function getSuitableHintContainer(element: Element): HTMLElement {
 
 			// We need to take into account the border since we need the distance from
 			// the target element to the padding box of the current element
-			const borderLeft = Number.parseInt(style.borderLeftWidth, 10);
-			const borderTop = Number.parseInt(style.borderTopWidth, 10);
+			const borderLeft = Number.parseInt(currentStyle.borderLeftWidth, 10);
+			const borderTop = Number.parseInt(currentStyle.borderTopWidth, 10);
 
 			if (
 				elementRect.x - rect.x + borderLeft >= minLeft &&
 				elementRect.y - rect.y + borderTop >= minTop
 			) {
-				resizeObserver.observe(candidate);
 				return candidate;
 			}
 
-			if (/hidden|auto/.test(style.overflow)) {
+			// If the current element is a scroll container or position fixed or sticky
+			// we can't go beyond as we need the hint to stay within those elements.
+			if (
+				((current.scrollHeight > current.clientHeight ||
+					current.scrollWidth > current.clientWidth) &&
+					/scroll|auto/.test(currentStyle.overflow)) ||
+				currentStyle.position === "fixed" ||
+				currentStyle.position === "sticky"
+			) {
+				if (/hidden|auto/.test(window.getComputedStyle(candidate).overflow)) {
+					return current;
+				}
+
+				return candidate;
+			}
+
+			if (/hidden|auto/.test(currentStyle.overflow)) {
 				candidate = undefined;
+			}
+
+			// If we get to the body and the candidate doesn't have enough space we
+			// need to place the hint in the candidate or the body if the candidate
+			// has overflow hidden or auto
+			if (current === document.body) {
+				return candidate ?? current;
 			}
 		}
 
