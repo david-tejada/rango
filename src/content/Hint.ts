a8a55760548bafaae43dac6a233dff94efd43aa9
David Tejada <davidxtejada@gmail.com>
2022-10-05 17:23:58 +0200

    Implement element wrappers

---
+++ src/content/Hint.ts
@@ -0,0 +1,233 @@
+import Color from "color";
+import { rgbaToRgb } from "../lib/rgbaToRgb";
+import { getSuitableHintContainer } from "./hints/getSuitableHintContainer";
+import { popHint, pushHint } from "./hints/hintsCache";
+import { setStyleProperties } from "./hints/setStyleProperties";
+import { getHintOption } from "./options/cacheHintOptions";
+import { getEffectiveBackgroundColor } from "./utils/getEffectiveBackgroundColor";
+import { getFirstTextNodeDescendant } from "./utils/nodeUtils";
+
+export class Hint {
+	readonly target: Element;
+	readonly outer: HTMLDivElement;
+	readonly inner: HTMLDivElement;
+	readonly container: Element;
+	positioned: boolean;
+	x?: number;
+	y?: number;
+	color: Color;
+	backgroundColor: Color;
+	firstTextNodeDescendant?: Text;
+	string?: string;
+
+	constructor(target: Element) {
+		this.target = target;
+		this.container = getSuitableHintContainer(target);
+
+		this.outer = document.createElement("div");
+		this.outer.className = "rango-hint-wrapper";
+
+		this.inner = document.createElement("div");
+		this.inner.className = "rango-hint";
+		this.outer.append(this.inner);
+
+		this.positioned = false;
+
+		// Initial styles for outer
+		const position =
+			window.getComputedStyle(this.container).display === "grid"
+				? "absolute"
+				: "relative";
+
+		setStyleProperties(this.outer, {
+			all: "initial",
+			position,
+			width: "0",
+			height: "0",
+			display: "none",
+		});
+
+		// Initial styles for inner
+
+		// Retrieve options
+		const hintFontSize = getHintOption("hintFontSize") as number;
+		const fontWeightOption = getHintOption("hintWeight") as
+			| "auto"
+			| "normal"
+			| "bold";
+		const subtleHints = getHintOption("hintStyle") === "subtle";
+		const subtleBackground =
+			subtleHints &&
+			window.getComputedStyle(this.target).display.includes("inline");
+
+		this.computeColors();
+
+		const outlineColor = new Color(this.color).alpha(0.3);
+
+		let fontWeight;
+		if (fontWeightOption === "auto") {
+			fontWeight =
+				this.backgroundColor.contrast(this.color) < 7 && hintFontSize < 14
+					? "bold"
+					: "normal";
+		} else {
+			fontWeight = `${fontWeightOption}`;
+		}
+
+		// We do this first as setStyleProperties doesn't guarantee going over the
+		// properties in order
+		setStyleProperties(this.inner, {
+			all: "initial",
+		});
+
+		setStyleProperties(this.inner, {
+			display: "none",
+			"z-index": "7000",
+			position: "absolute",
+			"border-radius": "20%",
+			"line-height": "1.25",
+			"font-family": "monospace",
+			"background-color": subtleBackground
+				? "transparent"
+				: this.backgroundColor.string(),
+			color: this.color.string(),
+			outline: subtleHints ? "0" : `1px solid ${outlineColor.string()}`,
+			"font-size": `${hintFontSize}px`,
+			"font-weight": fontWeight,
+			padding: "0 0.15em",
+		});
+	}
+
+	setBackgroundColor(color?: string) {
+		color ??= getEffectiveBackgroundColor(this.target);
+
+		setStyleProperties(this.inner, {
+			"background-color": color,
+		});
+	}
+
+	computeColors() {
+		this.firstTextNodeDescendant = getFirstTextNodeDescendant(this.target);
+		const backgroundColor = new Color(getEffectiveBackgroundColor(this.target));
+
+		const elementToGetColorFrom = this.firstTextNodeDescendant?.parentElement;
+		const colorString = window.getComputedStyle(
+			elementToGetColorFrom ?? this.target
+		).color;
+		let color = rgbaToRgb(new Color(colorString || "black"), backgroundColor);
+
+		if (!elementToGetColorFrom) {
+			if (backgroundColor.isDark() && color.isDark()) {
+				color = new Color("white");
+			}
+
+			if (backgroundColor.isLight() && color.isLight()) {
+				color = new Color("black");
+			}
+		}
+
+		/**
+		 * A contrast value of 2.5 might seem low but it is necessary to match the
+		 * look of some pages. Some pages use low contrast with big text and, in my
+		 * experience, it's more pleasant to keep the aspect of the page. Having in
+		 * mind that the text of the hints is not something that the user would need
+		 * to read continuously it might be acceptable to allow such a low contrast
+		 */
+		if (backgroundColor.contrast(color) < 2.5) {
+			color = backgroundColor.isLight()
+				? new Color("black")
+				: new Color("white");
+		}
+
+		this.backgroundColor = backgroundColor;
+		this.color = color;
+	}
+
+	updateColors() {
+		this.computeColors();
+		setStyleProperties(this.inner, {
+			"background-color": this.backgroundColor.string(),
+			color: this.color.string(),
+		});
+	}
+
+	position() {
+		if (!this.outer.isConnected) {
+			throw new Error("Trying to position an unconnected hint.");
+		}
+
+		const { x: targetX, y: targetY } = this.target.getBoundingClientRect();
+		const { x: outerX, y: outerY } = this.outer.getBoundingClientRect();
+		const x = targetX - outerX;
+		const y = targetY - outerY;
+
+		this.x = x;
+		this.y = y;
+
+		setStyleProperties(this.inner, {
+			left: `${x}px`,
+			top: `${y}px`,
+		});
+	}
+
+	flash(ms = 300) {
+		setStyleProperties(this.inner, {
+			"background-color": this.color.string(),
+			color: this.backgroundColor.string(),
+		});
+
+		setTimeout(() => {
+			this.computeColors();
+		}, ms);
+	}
+
+	claim() {
+		const string = popHint();
+
+		if (!string) {
+			throw new Error("No more hint strings available");
+		}
+
+		this.inner.textContent = string;
+		this.string = string;
+		this.container.append(this.outer);
+
+		setStyleProperties(this.outer, { display: "block" });
+
+		if (!this.positioned) {
+			this.position();
+			this.positioned = true;
+		}
+
+		setStyleProperties(this.inner, { display: "block" });
+
+		// This is here for debugging and testing purposes
+		if (process.env["NODE_ENV"] !== "production") {
+			this.outer.dataset["hint"] = string;
+			this.inner.dataset["hint"] = string;
+			if (this.target instanceof HTMLElement)
+				this.target.dataset["hint"] = string;
+		}
+
+		return string;
+	}
+
+	release(keepInCache = false) {
+		if (!this.inner.textContent || !this.string) {
+			throw new Error("HintError: Trying to release a null hint");
+		}
+
+		pushHint(this.string, keepInCache);
+		this.outer.remove();
+		this.inner.textContent = "";
+		this.string = undefined;
+		setStyleProperties(this.outer, { display: "none" });
+		setStyleProperties(this.inner, { display: "none" });
+
+		if (process.env["NODE_ENV"] !== "production") {
+			delete this.outer.dataset.hint;
+			delete this.inner.dataset.hint;
+			if (this.target instanceof HTMLElement) delete this.target.dataset.hint;
+		}
+	}
+}
---
+++ src/content/wrappers.ts
@@ -0,0 +1,400 @@
+import { debounce } from "../lib/debounceAndThrottle";
+import { createsStackingContext } from "./utils/createsStackingContext";
+import { isHintable } from "./utils/isHintable";
+import { isDisabled } from "./utils/isDisabled";
+import { isVisible } from "./utils/isVisible";
+import { getElementsFromOrigin } from "./utils/getElementsFromOrigin";
+import { cacheHints } from "./hints/hintsCache";
+import { getScrollContainer } from "./utils/getScrollContainer";
+import { BoundedIntersectionObserver } from "./BoundedIntersectionObserver";
+import { Hint } from "./Hint";
+
+// *** OBSERVERS ***
+
+// *** INTERSECTION OBSERVER ***
+
+export const scrollIntersectionObservers: Map<
+	Element | null,
+	BoundedIntersectionObserver
+> = new Map();
+
+export async function intersectionCallback(
+	entries: IntersectionObserverEntry[]
+) {
+	const amountIntersecting = entries.filter(
+		(entry) => entry.isIntersecting
+	).length;
+
+	await cacheHints(amountIntersecting);
+
+	for (const entry of entries) {
+		getWrapper(entry.target).intersect(entry.isIntersecting);
+	}
+}
+
+// *** MUTATION OBSERVER ***
+
+const mutationObserverConfig = {
+	attributes: true,
+	childList: true,
+	subtree: true,
+};
+
+const filterSelector = ":not(head, head *, .rango-hint-wrapper, .rango-hint)";
+
+const mutationCallback: MutationCallback = (mutationList) => {
+	const addedElements: Set<Element> = new Set();
+	const removedElements: Set<Element> = new Set();
+	const movedElements: Set<Element> = new Set();
+	let stylesMightHaveChanged = false;
+
+	for (const mutationRecord of mutationList) {
+		for (const node of mutationRecord.addedNodes) {
+			/**
+			 * When an element is moved from one parent to another we get a mutation
+			 * event with an entry with the elements in removedNodes and another one
+			 * with the	element in addedNodes. The issue here is that we don't get
+			 * any intersection event, so if we remove the element first from hintables
+			 * and then we add it again we are not gonna get any intersection and the
+			 * hint won't be created. Here we cancel out element that have been both
+			 * added and deleted.
+			 */
+			if (node instanceof Element && node.matches(filterSelector)) {
+				if (removedElements.has(node)) {
+					removedElements.delete(node);
+					movedElements.add(node);
+				} else {
+					addedElements.add(node);
+				}
+			}
+		}
+
+		for (const node of mutationRecord.removedNodes) {
+			if (node instanceof Element && node.matches(filterSelector)) {
+				removedElements.add(node);
+			}
+		}
+
+		if (
+			mutationRecord.attributeName &&
+			mutationRecord.target instanceof Element &&
+			mutationRecord.target.matches(filterSelector)
+		) {
+			stylesMightHaveChanged = true;
+
+			// We need to check if the element has changed its isHintable status
+			if (
+				["role", "contenteditable", "jsaction", "disabled"].includes(
+					mutationRecord.attributeName
+				)
+			) {
+				getWrapper(mutationRecord.target).updateIsHintable();
+				getWrapper(mutationRecord.target).updateShouldBeHinted();
+			}
+		}
+	}
+
+	for (const element of addedElements) {
+		addWrapper(element);
+	}
+
+	for (const element of removedElements) {
+		deleteWrapper(element);
+	}
+
+	for (const element of movedElements) {
+		getWrapper(element).elementWasMoved();
+	}
+
+	if (stylesMightHaveChanged) {
+		updateStyleAll();
+		updateShouldBeHintedAll();
+	}
+};
+
+export const mutationObserver = new MutationObserver(mutationCallback);
+
+// *** RESIZE OBSERVER ***
+
+const hintContainerResizeObserver = new ResizeObserver((entries) => {
+	for (const entry of entries) {
+		positionHintsInContainer(entry.target);
+	}
+});
+
+const hintablesResizeObserver = new ResizeObserver((entries) => {
+	for (const entry of entries) {
+		getWrapper(entry.target).updateShouldBeHinted();
+	}
+});
+
+const wrappersAll: Map<Element, ElementWrapper> = new Map();
+const wrappersHinted: Map<string, ElementWrapper> = new Map();
+
+// These methods adds the target and all of its descendants if they were
+// already created
+export function addWrapper(target: Element) {
+	const elements = getElementsFromOrigin(target);
+
+	for (const element of elements) {
+		if (!element.matches("rango-hint-wrapper, rango-hint")) {
+			const wrapper = wrappersAll.get(element) ?? new ElementWrapper(element);
+			wrappersAll.set(element, wrapper);
+
+			if (element.shadowRoot) {
+				mutationObserver.observe(element.shadowRoot, mutationObserverConfig);
+			}
+		}
+	}
+}
+
+export function getWrapper(key: Element | string): ElementWrapper;
+export function getWrapper(key: string[]): ElementWrapper[];
+export function getWrapper(
+	key: Element | string | string[]
+): ElementWrapper | ElementWrapper[] {
+	let result: ElementWrapper | ElementWrapper[] | undefined;
+
+	if (key instanceof Element) {
+		result = wrappersAll.get(key);
+	}
+
+	if (typeof key === "string") {
+		result = wrappersHinted.get(key);
+	}
+
+	if (Array.isArray(key)) {
+		result = [];
+		for (const string of wrappersHinted.keys()) {
+			if (key.includes(string)) {
+				result.push(wrappersHinted.get(string)!);
+			}
+		}
+	}
+
+	if (!result) {
+		if (key instanceof Element) {
+			result = new ElementWrapper(key);
+		} else {
+			// eslint-disable-next-line unicorn/prefer-type-error
+			throw new Error("Error finding ElementWrappers");
+		}
+	}
+
+	return result;
+}
+
+function deleteWrapper(target: Element) {
+	const elements = getElementsFromOrigin(target);
+	for (const element of elements) {
+		const wrapper = wrappersAll.get(element);
+
+		if (wrapper?.hint?.string) {
+			wrappersHinted.delete(wrapper.hint.string);
+			wrapper.hint.release();
+		}
+
+		wrappersAll.delete(element);
+	}
+}
+
+function updateStyleAll() {
+	for (const wrapper of wrappersHinted.values()) {
+		wrapper.hint?.updateColors();
+	}
+}
+
+const updateShouldBeHintedAll = debounce(() => {
+	for (const wrapper of wrappersAll.values()) {
+		if (wrapper.isHintable) {
+			wrapper.updateShouldBeHinted();
+		}
+	}
+}, 50);
+
+function positionHintsInContainer(container: Element) {
+	const hints: NodeListOf<HTMLDivElement> = container.querySelectorAll(
+		":scope > .rango-hint-wrapper > .rango-hint"
+	);
+
+	const strings = [...hints].map((hint) => hint.dataset["hint"]) as string[];
+	const wrappers = getWrapper(strings);
+
+	for (const wrapper of wrappers) {
+		wrapper.hint?.position();
+	}
+}
+
+export class ElementWrapper {
+	readonly element: Element;
+
+	isIntersecting: boolean;
+	isHintable: boolean;
+	shouldBeHinted: boolean;
+
+	// These properties are only needed for hintables
+	intersectionObserver?: BoundedIntersectionObserver;
+	scrollContainer?: HTMLElement | null;
+	effectiveBackgroundColor?: string;
+	hint?: Hint;
+
+	// Stacking contexts
+	createsStackingContext: boolean;
+	parentStackingContext?: ElementWrapper;
+	childrenStackingContexts?: Set<ElementWrapper>;
+
+	constructor(element: Element) {
+		this.element = element;
+		this.isIntersecting = false;
+
+		this.isHintable = isHintable(this.element);
+
+		if (this.isHintable) {
+			hintablesResizeObserver.observe(this.element);
+		}
+
+		this.shouldBeHinted =
+			this.isHintable && isVisible(this.element) && !isDisabled(this.element);
+
+		if (this.shouldBeHinted) {
+			this.observeIntersection();
+		}
+
+		this.createsStackingContext = createsStackingContext(element);
+
+		if (this.createsStackingContext) {
+			this.parentStackingContext = this.getParentStackingContext();
+
+			if (this.createsStackingContext) {
+				this.childrenStackingContexts = new Set();
+				this.parentStackingContext?.childrenStackingContexts?.add(this);
+			}
+		}
+	}
+
+	updateIsHintable() {
+		this.isHintable = isHintable(this.element);
+		if (this.isHintable) {
+			hintablesResizeObserver.observe(this.element);
+		}
+	}
+
+	updateShouldBeHinted() {
+		const newShouldBeHinted =
+			this.isHintable && isVisible(this.element) && !isDisabled(this.element);
+
+		if (newShouldBeHinted !== this.shouldBeHinted) {
+			if (newShouldBeHinted) {
+				this.observeIntersection();
+			} else {
+				if (this.hint?.string) {
+					this.hint.release();
+				}
+
+				this.unobserveIntersection();
+			}
+		}
+
+		this.shouldBeHinted = newShouldBeHinted;
+	}
+
+	observeIntersection() {
+		this.scrollContainer = getScrollContainer(this.element);
+
+		const root =
+			this.scrollContainer === document.documentElement ||
+			this.scrollContainer === document.body ||
+			!this.scrollContainer
+				? null
+				: this.scrollContainer;
+
+		const options: IntersectionObserverInit = {
+			root,
+			rootMargin: "300px",
+			threshold: 0,
+		};
+
+		this.intersectionObserver =
+			scrollIntersectionObservers.get(root) ??
+			new BoundedIntersectionObserver(intersectionCallback, options);
+
+		this.intersectionObserver.observe(this.element);
+
+		if (!scrollIntersectionObservers.has(root)) {
+			scrollIntersectionObservers.set(root, this.intersectionObserver);
+		}
+	}
+
+	unobserveIntersection() {
+		this.intersectionObserver?.unobserve(this.element);
+	}
+
+	intersect(isIntersecting: boolean) {
+		this.isIntersecting = isIntersecting;
+
+		if (this.isIntersecting && this.shouldBeHinted) {
+			this.hint ??= new Hint(this.element);
+			hintContainerResizeObserver.observe(this.hint.container);
+			wrappersHinted.set(this.hint.claim(), this);
+		} else if (this.hint?.string) {
+			wrappersHinted.delete(this.hint.string);
+			this.hint.release();
+		}
+	}
+
+	getParent(): ElementWrapper | undefined {
+		const parentElement = this.element.parentElement;
+		return parentElement ? wrappersAll.get(parentElement) : undefined;
+	}
+
+	getParentStackingContext(): ElementWrapper | undefined {
+		let current = this.getParent();
+		while (current) {
+			if (current.createsStackingContext) {
+				return current;
+			}
+
+			current = current.getParent();
+		}
+
+		return undefined;
+	}
+
+	recomputeStackingContext(isStackingContext: boolean) {
+		this.createsStackingContext = isStackingContext;
+
+		if (isStackingContext) {
+			this.childrenStackingContexts = new Set();
+			const parentStackingContext = this.getParentStackingContext();
+			if (parentStackingContext?.childrenStackingContexts) {
+				for (const childrenStackingContext of parentStackingContext.childrenStackingContexts) {
+					if (this.element.contains(childrenStackingContext.element)) {
+						this.childrenStackingContexts.add(childrenStackingContext);
+						parentStackingContext.childrenStackingContexts.delete(
+							childrenStackingContext
+						);
+					}
+				}
+			}
+		}
+	}
+
+	elementWasMoved() {
+		this.scrollContainer = getScrollContainer(this.element);
+
+		if (this.hint?.string) {
+			this.hint.release();
+		}
+
+		// if (this.createsStackingContext) {
+		// 	if (this.childrenStackingContexts) {
+		// 		for (const child of this.childrenStackingContexts) {
+		// 			this.parentStackingContext?.childrenStackingContexts?.add(child);
+		// 			this.childrenStackingContexts.delete(child);
+		// 		}
+		// 	}
+
+		// 	this.parentStackingContext = undefined;
+		// }
+	}
+}
