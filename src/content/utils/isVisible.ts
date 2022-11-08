4bcbf28976762848fedc0934c76804e4b7a460e0
David Tejada <davidxtejada@gmail.com>
2022-11-08 17:42:44 +0100

    Check if a close ancestor has opacity: 0 when checking if element is visible

--- src/content/utils/isVisible.ts
+++ src/content/utils/isVisible.ts
@@ -2,18 +2,22 @@ export function isVisible(element: Element): boolean {
 	const { visibility, opacity } = window.getComputedStyle(element);
 	const { width, height } = element.getBoundingClientRect();
 
-	if (
-		visibility === "hidden" ||
-		width < 5 ||
-		height < 5 ||
-		/**
-		 * An element could still be hidden if an ancestor has an opacity set but
-		 * because elements hidden this way are still clickable I don't think
-		 * we should worry too much about it
-		 */
-		Number.parseFloat(opacity) === 0
-	) {
+	if (visibility === "hidden" || width < 5 || height < 5 || opacity === "0") {
 		return false;
+	}
+
+	// Check if an element is hidden by a close ancestor having opacity: 0
+	let current = element.parentElement;
+	let counter = 0;
+
+	while (current && counter < 3) {
+		const { opacity } = window.getComputedStyle(current);
+		if (opacity === "0") {
+			return false;
+		}
+
+		current = current.parentElement;
+		counter++;
 	}
 
 	return true;
