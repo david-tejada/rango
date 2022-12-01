f7f5f91716d56a5cc3c3bcf7d77ffb40c16e8ef7
David Tejada <davidxtejada@gmail.com>
2022-12-01 17:28:03 +0100

    Improve duplicate hintable detection as :only-child doesn't takes into account text nodes

--- src/content/utils/isHintable.ts
+++ src/content/utils/isHintable.ts
@@ -31,7 +31,7 @@ for (const [index, selector] of excludeSelectors.entries()) {
 }
 
 const notSelector =
-	`:not([aria-hidden='true'], :is(${defaultSelector}) > *:only-child` +
+	":not([aria-hidden='true']" +
 	(excludeSelectorAll ? `, ${excludeSelectorAll}` : "") +
 	")";
 
@@ -39,6 +39,32 @@ const hintableSelector =
 	`:is(${defaultSelector})${notSelector}` +
 	(includeSelectorAll ? `, ${includeSelectorAll}` : "");
 
+function hasSignificantSiblings(target: Node) {
+	if (!target.parentNode) return false;
+
+	const significantSiblingsIncludingSelf = [
+		...target.parentNode.childNodes,
+	].filter(
+		(node) =>
+			node instanceof Element ||
+			(node instanceof Text && node.textContent && /\S/.test(node.textContent))
+	);
+
+	return significantSiblingsIncludingSelf.length > 1;
+}
+
+function isRedundant(target: Element) {
+	if (
+		!hasSignificantSiblings(target) &&
+		target.parentElement!.matches(hintableSelector) &&
+		!(includeSelectorAll && target.matches(includeSelectorAll))
+	) {
+		return true;
+	}
+
+	return false;
+}
+
 export function isHintable(target: Element): boolean {
-	return target.matches(hintableSelector);
+	return target.matches(hintableSelector) && !isRedundant(target);
 }
