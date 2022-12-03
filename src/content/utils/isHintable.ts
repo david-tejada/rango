faa13dcf2aaddcdbe8f1d6fb35c7e701a3bee60d
David Tejada <davidxtejada@gmail.com>
2022-12-03 17:23:24 +0100

    Don't display hints for labels if the control is visible

--- src/content/utils/isHintable.ts
+++ src/content/utils/isHintable.ts
@@ -1,3 +1,5 @@
+import { isVisible } from "./isVisible";
+
 const defaultSelector =
 	// Elements
 	"button, a, input, summary, textarea, select, option, label, " +
@@ -59,6 +61,14 @@ function isRedundant(target: Element) {
 		!hasSignificantSiblings(target) &&
 		target.parentElement!.matches(hintableSelector) &&
 		!(includeSelectorAll && target.matches(includeSelectorAll))
+	) {
+		return true;
+	}
+
+	if (
+		target instanceof HTMLLabelElement &&
+		target.control &&
+		isVisible(target.control)
 	) {
 		return true;
 	}
