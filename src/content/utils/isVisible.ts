e3e6aabc9002df3d02b40b355c9b4de8b389a0b3
David Tejada <davidxtejada@gmail.com>
2022-12-28 13:11:22 +0100

    Increase the amount of ancestors to check for opacity: 0 and add comments

--- src/content/utils/isVisible.ts
+++ src/content/utils/isVisible.ts
@@ -6,11 +6,17 @@ export function isVisible(element: Element): boolean {
 		return false;
 	}
 
-	// Check if an element is hidden by a close ancestor having opacity: 0
+	// Check if an element is hidden by a close ancestor having opacity: 0.
+	// Checking all the ancestors can take a significant amount of time (~50ms).
+	// Doing some research it seems like when a hintable element is hidden by an
+	// ancestor with an opacity of 0 it's usually the first or second ancestor.
+	// Checking four ancestors only adds about 10ms to 15ms. If in some rare case
+	// the ancestor with the opacity of 0 is beyond that is probably that the hint
+	// would be within that ancestor, so it would also remain hidden.
 	let current = element.parentElement;
 	let counter = 0;
 
-	while (current && counter < 3) {
+	while (current && counter < 4) {
 		const { opacity } = window.getComputedStyle(current);
 		if (opacity === "0") {
 			return false;
