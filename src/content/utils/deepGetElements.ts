30196e63a36c9b7ea8c8b9d8fe1f1db8d94aba44
David Tejada <davidxtejada@gmail.com>
2023-01-09 12:04:24 +0100

    Improve performance of deepGetElements

--- src/content/utils/deepGetElements.ts
+++ src/content/utils/deepGetElements.ts
@@ -5,30 +5,44 @@ export function deepGetElements(
 	includeRoot = true,
 	selector = ":not(.rango-hint-wrapper, .rango-hint, #rango-copy-paste-area)"
 ): Element[] {
+	const all = root.shadowRoot
+		? root.shadowRoot.querySelectorAll("*")
+		: root.querySelectorAll("*");
 	const result = includeRoot ? [root] : [];
-	const elements = root.shadowRoot
-		? [...root.shadowRoot.querySelectorAll("*")]
-		: [...root.querySelectorAll("*")];
 
-	for (const element of elements) {
-		if (element.shadowRoot) {
-			result.push(...deepGetElements(element));
-		} else {
-			result.push(element);
-		}
-	}
+	// This branch is more expensive so we only do it if some elements have
+	// shadowRoot
+	if ([...all].some((element) => element.shadowRoot)) {
+		const elements = root.shadowRoot
+			? [...root.shadowRoot.querySelectorAll("*")]
+			: [...root.querySelectorAll("*")];
 
-	return result.filter((element) => {
-		let matches;
-		try {
-			matches = element.matches(selector);
-		} catch (error: unknown) {
-			// This handles cases of invalid selectors
-			if (error instanceof DOMException) {
-				matches = false;
+		for (const element of elements) {
+			if (element.shadowRoot) {
+				result.push(...deepGetElements(element));
+			} else {
+				result.push(element);
 			}
 		}
 
-		return matches;
-	});
+		return result.filter((element) => {
+			let matches;
+			try {
+				matches = element.matches(selector);
+			} catch (error: unknown) {
+				// This handles cases of invalid selectors
+				if (error instanceof DOMException) {
+					matches = false;
+				}
+			}
+
+			return matches;
+		});
+	}
+
+	const filtered = root.shadowRoot
+		? root.shadowRoot.querySelectorAll(selector)
+		: root.querySelectorAll(selector);
+	result.push(...filtered);
+	return result;
 }
