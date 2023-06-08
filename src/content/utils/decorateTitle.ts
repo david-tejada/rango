cc5c0910db4675c634b7346dbd0472135e5c984b
David Tejada <davidxtejada@gmail.com>
2023-06-08 16:55:36 +0200

    Fix some occurrences of the title decorations being duplicated
    Also fix the title decorations not being reapplied when the title changes without the url changing

--- src/content/utils/decorateTitle.ts
+++ src/content/utils/decorateTitle.ts
@@ -27,21 +27,43 @@ async function getTitleSuffix() {
 	const urlInTitle = await retrieve("urlInTitle");
 
 	if (urlInTitle) {
-		lastUrlAdded = window.location.href;
 		return ` - ${window.location.href}`;
 	}
 
 	return "";
 }
 
+async function removeDecorations(prefix: string) {
+	if (document.title.startsWith(prefix)) {
+		document.title = document.title.slice(prefix.length);
+	}
+
+	if (await retrieve("urlInTitle")) {
+		const possibleSuffix = ` - ${lastUrlAdded ?? window.location.href}`;
+		if (document.title.endsWith(possibleSuffix)) {
+			document.title = document.title.slice(0, -possibleSuffix.length);
+		}
+	}
+}
+
 async function decorateTitle() {
+	const prefix = await getTitlePrefix();
+	const suffix = await getTitleSuffix();
+
+	// Make extra sure we don't duplicate the prefix or suffix. Prevents
+	// decorations from being added multiple times when the extension is updated
+	// and in some other difficult to reproduce situations.
+	await removeDecorations(prefix);
+
 	if (document.title !== titleAfterDecoration) {
 		titleBeforeDecoration = document.title;
 	}
 
-	const prefix = await getTitlePrefix();
-	const suffix = await getTitleSuffix();
 	document.title = prefix + titleBeforeDecoration! + suffix;
+
+	if (suffix) {
+		lastUrlAdded = window.location.href;
+	}
 
 	titleAfterDecoration = document.title;
 }
@@ -50,31 +72,16 @@ const throttledMutationCallback = throttle(async () => {
 	// We need to check if the url has changed every time there is a mutation.
 	// The URL could be changed using something like history.pushState and
 	// sometimes the title doesn't even change (issue #75).
-	if (window.location.href !== lastUrlAdded) {
+	if (
+		window.location.href !== lastUrlAdded ||
+		document.title !== titleAfterDecoration
+	) {
 		await decorateTitle();
 	}
 }, 500);
 
-async function removeDecorations() {
-	const prefixRe = /^[a-z]{1,2} \| /i;
-	document.title = document.title.replace(prefixRe, "");
-
-	if (await retrieve("urlInTitle")) {
-		const suffixRe = new RegExp(` - ${window.location.href}$`);
-		document.title = document.title.replace(suffixRe, "");
-	}
-}
-
 export async function initTitleDecoration() {
 	const urlInTitle = getCachedSetting("urlInTitle");
-
-	// Remove decorations when the content script starts. Prevents decorations
-	// from being added multiple times when the extension is updated. This can
-	// happen in development and also if the user updates the extension by
-	// clicking the button to check for updates.
-	if (titleBeforeDecoration === undefined) {
-		await removeDecorations();
-	}
 
 	await decorateTitle();
 
