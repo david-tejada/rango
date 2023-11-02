94c178c5fdcfb3933b8cdc93b1dabfe2a85f08b9
David Tejada <davidxtejada@gmail.com>
2023-11-02 11:30:33 +0100

    Remove the decorations added by Rango when saving bookmarks (#226)

--- src/background/setup/initBackgroundScript.ts
+++ src/background/setup/initBackgroundScript.ts
@@ -2,6 +2,7 @@ import browser from "webextension-polyfill";
 import { retrieve, store } from "../../common/storage";
 import { urls } from "../../common/urls";
 import { watchNavigation } from "../hints/watchNavigation";
+import { sendRequestToContent } from "../messaging/sendRequestToContent";
 import { createContextMenus } from "../misc/createContextMenus";
 import { initTabMarkers } from "../misc/tabMarkers";
 import { setBrowserActionIcon } from "../utils/browserAction";
@@ -63,3 +64,37 @@ browser.runtime.onStartup.addListener(async () => {
 	// In Safari we need to create the menus every time the browser starts.
 	if (isSafari()) await createContextMenus();
 });
+
+/**
+ * Strip the tab marker and url added by Rango from the title of the bookmark.
+ */
+async function resetBookmarkTitle(id: string) {
+	const includeTabMarkers = await retrieve("includeTabMarkers");
+	const urlInTitle = await retrieve("urlInTitle");
+
+	if (includeTabMarkers || urlInTitle) {
+		try {
+			const titleBeforeDecorations = (await sendRequestToContent({
+				type: "getTitleBeforeDecoration",
+			})) as string | undefined;
+
+			if (titleBeforeDecorations) {
+				// We remove the event listener temporarily so it doesn't trigger when
+				// we update the title.
+				browser.bookmarks?.onChanged.removeListener(resetBookmarkTitle);
+				await browser.bookmarks.update(id, { title: titleBeforeDecorations });
+				browser.bookmarks?.onChanged.addListener(resetBookmarkTitle);
+			}
+		} catch {
+			// Do nothing. The user might be modifying the bookmark manually. In that
+			// case sendRequestToContent would fail.
+		}
+	}
+}
+
+// We use optional chaining because this isn't supported in Safari.
+browser.bookmarks?.onCreated.addListener(resetBookmarkTitle);
+
+// In Chrome the bookmark is created when we open the dialog and updated when we
+// submit.
+browser.bookmarks?.onChanged.addListener(resetBookmarkTitle);
