6ca150a2c0db79293357f213906c3e7ed8e36b71
David Tejada <davidxtejada@gmail.com>
2024-04-25 11:22:36 +0200

    Remove the need to enable the setting "Always compute hintables" for using fuzzy text clicking

--- src/content/actions/runActionOnTextMatchedElement.ts
+++ src/content/actions/runActionOnTextMatchedElement.ts
@@ -1,63 +1,104 @@
 import Fuse from "fuse.js";
-import { ElementWrapper } from "../../typings/ElementWrapper";
-import { notify } from "../notify/notify";
+import { RangoActionWithTargets } from "../../typings/RangoAction";
 import { getCachedSetting } from "../settings/cacheSettings";
 import { getToggles } from "../settings/toggles";
-import { getAllWrappers } from "../wrappers/wrappers";
-import { RangoActionWithTargets } from "../../typings/RangoAction";
+import { deepGetElements } from "../utils/deepGetElements";
+import { isHintable } from "../utils/isHintable";
 import { isVisible } from "../utils/isVisible";
+import { getOrCreateWrapper } from "../wrappers/ElementWrapperClass";
+import { getAllWrappers } from "../wrappers/wrappers";
 import { runRangoActionWithTarget } from "./runRangoActionWithTarget";
 
-let textMatchedWrapper: ElementWrapper | undefined;
+let textMatchedElement: Element | undefined;
+let shouldScrollIntoView = false;
+
+type Hintable = {
+	element: Element;
+	isIntersectingViewport: boolean;
+	trimmedTextContent: string;
+};
+
+function trimTextContent(element: Element) {
+	return element.textContent?.replace(/\d/g, "").trim() ?? "";
+}
+
+async function getHintables() {
+	// Hints are on, there will be wrappers for the hintable elements
+	if (getToggles().computed || getCachedSetting("alwaysComputeHintables")) {
+		return getAllWrappers()
+			.filter((wrapper) => wrapper.isHintable && isVisible(wrapper.element))
+			.map((wrapper) => ({
+				element: wrapper.element,
+				isIntersectingViewport: wrapper.isIntersectingViewport,
+				trimmedTextContent: trimTextContent(wrapper.element),
+			}));
+	}
+
+	const hintables: Hintable[] = [];
+	const elements = deepGetElements(document.body, true);
+
+	let resolveIntersection: (value: unknown) => void | undefined;
+
+	const intersectionPromise = new Promise((resolve) => {
+		resolveIntersection = resolve;
+	});
+
+	const intersectionObserver = new IntersectionObserver((entries) => {
+		for (const entry of entries) {
+			if (isHintable(entry.target) && isVisible(entry.target)) {
+				hintables.push({
+					element: entry.target,
+					isIntersectingViewport: entry.isIntersecting,
+					trimmedTextContent: trimTextContent(entry.target),
+				});
+			}
+		}
+
+		if (resolveIntersection) resolveIntersection(true);
+	});
+
+	for (const element of elements) {
+		intersectionObserver.observe(element);
+	}
+
+	await intersectionPromise;
+	return hintables;
+}
 
 export async function matchElementByText(
 	text: string,
 	prioritizeViewport: boolean
 ) {
-	if (!getToggles().computed && !getCachedSetting("alwaysComputeHintables")) {
-		await notify(
-			'Enable the setting "Always compute hintable elements" if you want this command to work while the hints are off.',
-			{ type: "warning" }
-		);
-		return;
-	}
+	const hintables = await getHintables();
 
-	const hintablesWithTextContent = getAllWrappers()
-		.filter((w) => w.isHintable && isVisible(w.element))
-		.map((wrapper) => ({
-			wrapper,
-			textContent: wrapper.element.textContent?.replace(/\d/g, "").trim(),
-		}));
-
-	const fuse = new Fuse(hintablesWithTextContent, {
-		keys: ["textContent"],
+	const fuse = new Fuse(hintables, {
+		keys: ["trimmedTextContent"],
 		ignoreLocation: true,
 		includeScore: true,
 		threshold: 0.3,
 	});
 
-	const sortedMatches = fuse.search(text).sort((a, b) => {
-		if (prioritizeViewport) {
-			if (
-				a.item.wrapper.isIntersectingViewport &&
-				!b.item.wrapper.isIntersectingViewport
-			) {
-				return -1;
-			}
+	const matches = fuse.search(text);
 
-			if (
-				!a.item.wrapper.isIntersectingViewport &&
-				b.item.wrapper.isIntersectingViewport
-			) {
-				return 1;
-			}
-		}
+	const sortedMatches = prioritizeViewport
+		? matches.sort((a, b) => {
+				if (a.item.isIntersectingViewport && !b.item.isIntersectingViewport) {
+					return -1;
+				}
 
-		return a.score! - b.score!;
-	});
+				if (!a.item.isIntersectingViewport && b.item.isIntersectingViewport) {
+					return 1;
+				}
+
+				return a.score! - b.score!;
+		  })
+		: matches;
 
 	const bestMatch = sortedMatches[0];
-	textMatchedWrapper = bestMatch?.item.wrapper;
+	if (bestMatch?.item) {
+		textMatchedElement = bestMatch.item.element;
+		shouldScrollIntoView = !bestMatch.item.isIntersectingViewport;
+	}
 
 	return bestMatch?.score;
 }
@@ -65,9 +106,20 @@ export async function matchElementByText(
 export async function executeActionOnTextMatchedElement(
 	actionType: RangoActionWithTargets["type"]
 ) {
-	if (textMatchedWrapper) {
-		await runRangoActionWithTarget({ type: actionType, target: [] }, [
-			textMatchedWrapper,
-		]);
+	if (textMatchedElement) {
+		const wrapper = getOrCreateWrapper(textMatchedElement, false);
+
+		// If the element is outside of the viewport we need to scroll the element
+		// into view in order to get the topmost element, which is the one we need
+		// to click on.
+		if (shouldScrollIntoView) {
+			textMatchedElement.scrollIntoView({
+				behavior: "instant",
+				block: "center",
+				inline: "center",
+			});
+		}
+
+		await runRangoActionWithTarget({ type: actionType, target: [] }, [wrapper]);
 	}
 }
