dcad37a45f2daeb67271359b55dea38a77afa0e2
David Tejada <davidxtejada@gmail.com>
2024-10-24 10:17:03 +0200

    Modify isValidMessage to accept messages without data

--- src/common/messaging/isValidMessage.ts
+++ src/common/messaging/isValidMessage.ts
@@ -5,8 +5,6 @@ export function isValidMessage(
 		typeof message === "object" &&
 		message !== null &&
 		"messageId" in message &&
-		typeof message.messageId === "string" &&
-		"data" in message &&
-		(typeof message.data === "object" || message.data === undefined)
+		typeof message.messageId === "string"
 	);
 }
