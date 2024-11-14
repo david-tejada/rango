72a731dc5ed523dd40b435e891f634810a4988e2
David Tejada <davidxtejada@gmail.com>
2024-11-14 09:59:10 +0100

    Improve types in commandBroker

--- src/background/commands/commandBroker.ts
+++ src/background/commands/commandBroker.ts
@@ -1,28 +1,29 @@
 import type { ActionMap } from "../../typings/Action";
 import type { TalonAction } from "../../typings/TalonAction";
 
-type CommandHandler = (
-	args: ActionMap[keyof ActionMap]
+type CommandHandler<T extends keyof ActionMap> = (
+	args: ActionMap[T]
 ) => Promise<undefined | TalonAction | TalonAction[] | "noResponse">;
 
-const commandHandlers = new Map<keyof ActionMap, CommandHandler>();
+const commandHandlers = new Map<
+	keyof ActionMap,
+	CommandHandler<keyof ActionMap>
+>();
 
 export function onCommand<T extends keyof ActionMap>(
 	name: T,
-	handler: (
-		args: ActionMap[T]
-	) => Promise<void | TalonAction | TalonAction[] | "noResponse">
+	handler: CommandHandler<T>
 ) {
 	if (commandHandlers.has(name)) {
 		throw new Error("Only one command handler per command can be registered.");
 	}
 
-	commandHandlers.set(name, handler as CommandHandler);
+	commandHandlers.set(name, handler as CommandHandler<keyof ActionMap>);
 }
 
-export async function handleCommand<Name extends keyof ActionMap>(
-	name: Name,
-	args: ActionMap[keyof ActionMap]
+export async function handleCommand<T extends keyof ActionMap>(
+	name: T,
+	args: ActionMap[T]
 ) {
 	const commandHandler = commandHandlers.get(name);
 	if (!commandHandler) throw new Error(`Command "${name}" doesn't exist.`);
