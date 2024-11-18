52abacfe815a8dc1906690e5687c26e551111c71
David Tejada <davidxtejada@gmail.com>
2024-11-18 16:22:00 +0100

    Refactor and add comments to hintsCache

--- src/content/hints/hintsCache.ts
+++ src/content/hints/hintsCache.ts
@@ -8,45 +8,70 @@ import {
 	releaseHints,
 } from "./hintsRequests";
 
-let mainCache: string[] = [];
-let additionalCache: string[] = [];
-let returnedHints: string[] = [];
+/**
+ * Hints that have been claimed for elements within the viewport. These hints
+ * cannot be reclaimed by other frames.
+ */
+const mainCache: string[] = [];
 
-// This function is called from the intersection observer callback on every
-// intersection.
-export async function cacheHints(necessary: number, additional: number) {
-	const total = necessary + additional;
+/**
+ * Hints that have been claimed for elements that are intersecting but not
+ * within the viewport. These hints are susceptible to be reclaimed if a
+ * different frame requires more.
+ */
+const additionalCache: string[] = [];
 
-	const hintsToRequest = total - returnedHints.length;
+/**
+ * Hints that have been returned to the cache using `pushHint`.
+ */
+const returnedHints: string[] = [];
 
-	if (returnedHints.length > 0) {
-		saveHintsToCache(returnedHints.splice(0, returnedHints.length));
+/**
+ * Caches hints that can be later `popped` when a wrapper needs them.
+ *
+ * @param necessaryCount - The number of hints that are absolutely necessary.
+ * They are intended for elements that are within the viewport.
+ * @param additionalCount - The number of hints that are additional to the
+ * necessary ones. They are intended for elements that are intersecting but not
+ * within the viewport.
+ */
+export async function cacheHints(
+	necessaryCount: number,
+	additionalCount: number
+) {
+	const totalCount = necessaryCount + additionalCount;
+	const requestCount = totalCount - returnedHints.length;
+
+	// Save any previously returned hints
+	saveHintsToCache([...returnedHints]);
+	returnedHints.length = 0;
+
+	// If we have excess hints, return them to the stack
+	if (requestCount <= 0) {
+		await releaseHints(mainCache.splice(0, -requestCount));
+		return;
 	}
 
-	if (hintsToRequest > 0) {
-		const minimumAmountRequired = Math.min(hintsToRequest, necessary);
-		const hints = await claimHints(hintsToRequest);
+	const minimumCount = Math.min(requestCount, necessaryCount);
+	const hintsClaimed = await claimHints(requestCount);
 
-		if (hints.length < minimumAmountRequired) {
-			// If there are not enough hints available we try to reclaim those hints
-			// that are outside of the viewport in the same frame (for speed)
-			hints.push(...reclaimHints(necessary - hints.length));
-		}
-
-		if (hints.length < minimumAmountRequired) {
-			// If after that there're still not enough hints available we reclaim
-			// hints that are outside of the viewport from others frames
-			const additionalHints = await reclaimHintsFromOtherFrames(
-				necessary - hints.length
-			);
-			saveHintsToCache(additionalHints);
-		}
-
-		saveHintsToCache(hints, necessary);
-	} else {
-		// We return the excess hints to the stack
-		await releaseHints(mainCache.splice(0, -hintsToRequest));
+	// If there are not enough hints available we try to reclaim those hints
+	// that are outside of the viewport in the same frame (for speed)
+	if (hintsClaimed.length < minimumCount) {
+		const reclaimedHints = reclaimHints(necessaryCount - hintsClaimed.length);
+		hintsClaimed.push(...reclaimedHints);
 	}
+
+	// If after that there're still not enough hints available we reclaim
+	// hints that are outside of the viewport from others frames
+	if (hintsClaimed.length < minimumCount) {
+		const reclaimedHints = await reclaimHintsFromOtherFrames(
+			necessaryCount - hintsClaimed.length
+		);
+		hintsClaimed.push(...reclaimedHints);
+	}
+
+	saveHintsToCache(hintsClaimed, necessaryCount);
 }
 
 export function popHint(): string | undefined {
@@ -65,14 +90,33 @@ export function pushHint(hint: string) {
 	returnedHints.push(hint);
 }
 
-function saveHintsToCache(hints: string[], toMain?: number) {
-	const hintsToAdditional = [...hints];
-	const hintsToMain: string[] = toMain
-		? hintsToAdditional.splice(-toMain, toMain)
-		: hintsToAdditional.splice(0, hints.length);
+/**
+ * Saves hints to the cache.
+ *
+ * @param hints - The hints to save.
+ * @param toMain - The number of hints that should be saved to the main cache.
+ */
+export function saveHintsToCache(hints: string[], toMain = 0) {
+	const hintsSorted = [...hints].sort(
+		(a, b) => b.length - a.length || b.localeCompare(a)
+	);
+
+	const [hintsToAdditional, hintsToMain] = [
+		hintsSorted.slice(0, -toMain),
+		hintsSorted.slice(-toMain),
+	];
 
 	mainCache.push(...hintsToMain);
 	additionalCache.push(...hintsToAdditional);
+
+	sortCache();
+}
+
+/**
+ * Sorts the hints in the cache.
+ * The sorted array will be like this: `["zz", "zy", "zx", ..., "c", "b", "a"]`
+ */
+function sortCache() {
 	mainCache.sort((a, b) => b.length - a.length || b.localeCompare(a));
 	additionalCache.sort((a, b) => b.length - a.length || b.localeCompare(a));
 }
@@ -81,17 +125,40 @@ export function reclaimHintsFromCache(amount: number) {
 	return additionalCache.splice(-amount, amount);
 }
 
+export async function replaceHintsInCache(newHints: string[]) {
+	const mainCacheCount = mainCache.length;
+	const additionalCacheCount = additionalCache.length;
+	const returnHintsCount = returnedHints.length;
+
+	console.log(returnedHints);
+
+	console.log({ mainCacheCount, additionalCacheCount, returnHintsCount });
+
+	await clearHintsCache();
+
+	mainCache.push(...newHints.splice(0, mainCacheCount));
+	additionalCache.push(...newHints.splice(0, additionalCacheCount));
+	returnedHints.push(...newHints.splice(0, returnHintsCount));
+
+	console.log(returnedHints);
+}
+
 export async function clearHintsCache() {
-	mainCache = [];
-	additionalCache = [];
-	returnedHints = [];
+	mainCache.length = 0;
+	additionalCache.length = 0;
+	returnedHints.length = 0;
 
 	clearHintsInFrame();
 
 	if (isMainFrame()) await initStack();
 }
 
-// For debugging purposes
+/**
+ * Returns the hints in the cache. This function is just used for debugging
+ * purposes.
+ *
+ * @returns The hints in the cache.
+ */
 export function getHintsCache() {
 	return { main: mainCache, additional: additionalCache };
 }
