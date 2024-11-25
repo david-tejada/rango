34fc4f10638659cb123882d9f63d59269d14dcd9
David Tejada <davidxtejada@gmail.com>
2024-11-25 09:58:58 +0100

    Small refactor and commenting

--- src/background/actions/focusPreviousTab.ts
+++ src/background/actions/focusPreviousTab.ts
@@ -1,18 +1,19 @@
 import browser from "webextension-polyfill";
-import { assertDefined } from "../../typings/TypingUtils";
 import { retrieve } from "../../common/storage";
 
+/**
+ * Focuses the tab in the current window that was most recently active.
+ */
 export async function focusPreviousTab() {
 	const tabsByRecency = await retrieve("tabsByRecency");
 	const currentWindow = await browser.windows.getCurrent();
-	assertDefined(currentWindow.id);
-	const previousTabs = tabsByRecency.get(currentWindow.id);
-	assertDefined(previousTabs);
+
+	const previousTabs = tabsByRecency.get(currentWindow.id!);
+	if (!previousTabs) throw new Error("Unable to find previously focused tabs.");
 
 	// The last tab id is the current tab so we need to get the one before that
 	const previousTabId = previousTabs.at(-2);
+	if (!previousTabId) throw new Error("Unable to find previously focused tab.");
 
-	if (previousTabId) {
-		await browser.tabs.update(previousTabId, { active: true });
-	}
+	await browser.tabs.update(previousTabId, { active: true });
 }
