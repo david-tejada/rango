c5714d1046a53901b5db74c855c9a32c25975193
David Tejada <davidxtejada@gmail.com>
2024-12-09 15:57:39 +0100

    Remove unused testing mocks

--- e2e/utils/mockResizeObserver.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-class MockResizeObserver {
-	observe() {}
-	unobserve() {}
-	disconnect() {}
-}
-
-globalThis.ResizeObserver = MockResizeObserver;
