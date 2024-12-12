7e5a667f3a4a0c7dfb787eec291b4feb6a3080fc
David Tejada <davidxtejada@gmail.com>
2024-12-12 13:11:52 +0100

    Fix toast not working when extension reloads

--- src/content/feedback/notify.tsx
+++ src/content/feedback/notify.tsx
@@ -1,8 +1,8 @@
 import * as toastCSS from "bundle-text:./toast/Toast.css";
 import * as toastifyCSS from "bundle-text:./toast/toastify.css";
 import * as toastTogglesCSS from "bundle-text:./toast/ToastTogglesMessage.css";
-import { createRoot } from "react-dom/client";
-import { type ToastOptions, toast } from "react-toastify";
+import { createRoot, type Root } from "react-dom/client";
+import { toast, type ToastOptions } from "react-toastify";
 import { createElement } from "../dom/utils";
 import { getSetting } from "../settings/settingsManager";
 import { isCurrentTab, isMainFrame } from "../setup/contentScriptContext";
@@ -11,33 +11,41 @@ import { ToastIcon } from "./toast/ToastIcon";
 import { ToastMessage } from "./toast/ToastMessage";
 import { TogglesStatusMessage } from "./toast/ToastTogglesMessage";
 
+let toastRoot: Root | undefined;
+
 function renderToast() {
 	let shadowHost = document.querySelector("#rango-toast");
 
+	if (shadowHost && toastRoot) {
+		return;
+	}
+
 	if (!shadowHost) {
-		const toastifyStyles = createElement("style", {
-			id: "toastify-styles",
-			textContent: toastifyCSS,
-		});
-
-		const toastStyles = createElement("style", {
-			id: "toast-styles",
-			textContent: toastCSS,
-		});
-
-		const toastTogglesStyles = createElement("style", {
-			id: "toast-toggles-styles",
-			textContent: toastTogglesCSS,
-		});
-
-		shadowHost = createElement("div", {
-			id: "rango-toast",
-		});
+		shadowHost = createElement("div", { id: "rango-toast" });
 		const shadowRoot = shadowHost.attachShadow({ mode: "open" });
-		shadowRoot.append(toastifyStyles, toastStyles, toastTogglesStyles);
 
-		createRoot(shadowRoot).render(<Toast />);
+		shadowRoot.append(
+			createElement("style", {
+				id: "toastify-styles",
+				textContent: toastifyCSS,
+			}),
+			createElement("style", {
+				id: "toast-styles",
+				textContent: toastCSS,
+			}),
+			createElement("style", {
+				id: "toast-toggles-styles",
+				textContent: toastTogglesCSS,
+			})
+		);
+
 		document.body.append(shadowHost);
+	}
+
+	const shadowRoot = shadowHost.shadowRoot;
+	if (shadowRoot) {
+		toastRoot = createRoot(shadowRoot);
+		toastRoot.render(<Toast />);
 	}
 }
 
