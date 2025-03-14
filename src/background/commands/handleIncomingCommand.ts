9371eb62813260472a0b9135d7749f949e49764b
David Tejada <davidxtejada@gmail.com>
2025-03-14 16:07:43 +0100

    Add notification to restart the browser on content script error when the extension recently updated

--- src/background/commands/handleIncomingCommand.ts
+++ src/background/commands/handleIncomingCommand.ts
@@ -1,3 +1,4 @@
+import { store } from "../../common/storage/store";
 import { UnreachableContentScriptError } from "../messaging/UnreachableContentScriptError";
 import { handleCommand } from "./commandHandler";
 import { readRequest, writeResponse } from "./requestAndResponse";
@@ -19,9 +20,14 @@ export async function handleIncomingCommand() {
 		}
 	} catch (error: unknown) {
 		if (error instanceof UnreachableContentScriptError) {
+			const extensionRecentlyUpdated = await store.get(
+				"extensionRecentlyUpdated"
+			);
+
 			// Rethrow the error with a more descriptive message.
-			const message =
-				"Unable to run command. This command can't run on browser system pages (settings, new tabs, extensions, or other privileged pages).";
+			const message = extensionRecentlyUpdated
+				? "Unable to run command. Rango was recently updated, please restart the browser or refresh the current page."
+				: "Unable to run command. This command can't run on browser system pages (settings, new tabs, extensions, or other privileged pages).";
 			await writeResponse({ name: "throwError", message });
 			throw new UnreachableContentScriptError(message);
 		}
