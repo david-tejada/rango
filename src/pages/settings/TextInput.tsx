7b4916864deb0afa8b6654176e71d9fc9c8b47be
Brian Call <bacall213@users.noreply.github.com>
2025-04-22 17:56:18 -0700

    Update TextInput.tsx
    - Add updated validation bits

--- src/pages/settings/TextInput.tsx
+++ src/pages/settings/TextInput.tsx
@@ -1,10 +1,13 @@
 import { useId } from "react";
 import "./Input.css";
+import { Alert } from "./Alert";
 
 type InputProps = {
 	readonly label: string;
 	readonly defaultValue: string | number;
 	readonly isValid?: boolean;
+	readonly validationMessage?: string;
+	readonly isDisabled?: boolean;
 	readonly children?: React.ReactNode;
 	onChange(value: string): void;
 	onBlur?(): void;
@@ -13,12 +16,19 @@ type InputProps = {
 export function TextInput({
 	label,
 	defaultValue,
-	isValid: valid,
+	isValid,
+	validationMessage,
+	isDisabled,
 	onChange,
 	onBlur,
 	children,
 }: InputProps) {
 	const id = useId();
+
+	const errorMessage =
+		validationMessage === "Invalid input"
+			? "Incorrect color string. Use a CSS color string."
+			: validationMessage;
 
 	return (
 		<div className="Input">
@@ -28,7 +38,8 @@ export function TextInput({
 					value={defaultValue}
 					id={id}
 					type="text"
-					data-is-valid={valid}
+					data-is-valid={isValid}
+					disabled={isDisabled}
 					onChange={(event) => {
 						onChange(event.target.value);
 					}}
@@ -37,6 +48,11 @@ export function TextInput({
 						if (onBlur) onBlur();
 					}}
 				/>
+				{!isValid && !isDisabled && (
+					<Alert type="error" elementId={id}>
+						{errorMessage}
+					</Alert>
+				)}
 				{children}
 			</div>
 		</div>
